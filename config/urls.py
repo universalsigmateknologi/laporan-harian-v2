01292634ff407f337674e6525f6059df4e5f1418
universalsigmateknologi <universalsigmateknologi@gmail.com>
2026-06-15 12:49:25 +0700

    bismillah

--- config/urls.py
+++ config/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
 ]
 
 # Sajikan file media di mode development
-if settings.DEBUG:
+if settings.local.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     
