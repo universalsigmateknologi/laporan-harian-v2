24923133553c4990c9100d4350a609484c3898df
universalsigmateknologi <universalsigmateknologi@gmail.com>
2026-06-15 11:49:35 +0700

    configurasi for local

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 
 
 # Quick-start development settings - unsuitable for production
