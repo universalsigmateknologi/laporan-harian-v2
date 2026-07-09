7107d6a133b95e4daef2f881486a5c662a6c081d
universalsigmateknologi <universalsigmateknologi@gmail.com>
2026-07-09 08:52:02 +0700

    coba lagi

--- config/urls.py
+++ config/urls.py
@@ -34,6 +34,6 @@ urlpatterns = [
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if getattr(settings, "DEBUG", False):
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
